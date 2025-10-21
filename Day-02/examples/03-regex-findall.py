222840be30ea9b25d8ec0d60a0a57a7e0389e1fd
Nithya-v-kumar-03 <nithyavkumar705@gmail.com>
2025-10-21 11:27:29 +0530

    Update 03-regex-findall.py

--- Day-02/examples/03-regex-findall.py
+++ Day-02/examples/03-regex-findall.py
@@ -1,10 +1,19 @@
 import re
 
 text = "The quick brown fox"
-pattern = r"brown"
+pattern = r"brown"                     
 
 search = re.search(pattern, text)
 if search:
-    print("Pattern found:", search.group())
+    print("Pattern found:", search.group())     //search.group() returns the exact substring of text that matched pattern.
 else:
     print("Pattern not found")
+---------------------------------------------------------------------
+With a raw string (prefix r or R), the literal tells Python: “Don’t interpret backslashes as the start of escape sequences — treat them more literally.” 
+
+# Example 1: newline escape
+a = "A\nB"
+b = r"A\nB"
+print(a)   # A
+           # B
+print(b)   # A\nB
