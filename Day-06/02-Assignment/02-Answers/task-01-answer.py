b9898da1ea03ba21b9ea89e4434af017570d2f22
Nithya-v-kumar-03 <nithyavkumar705@gmail.com>
2025-10-23 09:02:31 +0530

    Update task-01-answer.py

--- Day-06/02-Assignment/02-Answers/task-01-answer.py
+++ Day-06/02-Assignment/02-Answers/task-01-answer.py
@@ -10,3 +10,28 @@ print("Sum:", sum_result)
 print("Difference:", difference_result)
 print("Product:", product_result)
 print("Quotient:", quotient_result)
+
+---------------------------------------------------------------
+
+def add(num1 , num2):
+     a= num1 + num2
+     return a
+
+def sub(num1 , num2):
+     return num1 - num2
+
+def mul(num1 , num2):
+     return num1 * num2
+
+def div(num1 , num2):
+     return  num1 / num2
+
+def divfloat(num1 , num2):
+    return num1 // num2
+
+
+print(add(3,5))
+print(sub(3,5))
+print(mul(5,8))
+print(div(16 ,13))
+print(divfloat(17,13))
