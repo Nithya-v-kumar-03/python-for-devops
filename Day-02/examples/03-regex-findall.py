75c173686781df5640857513eacbe4252196bd8a
Nithya-v-kumar-03 <nithyavkumar705@gmail.com>
2025-11-28 06:55:52 +0530

    Update 03-regex-findall.py

--- Day-02/examples/03-regex-findall.py
+++ Day-02/examples/03-regex-findall.py
@@ -6,7 +6,7 @@ pattern = r"brown"
 search = re.search(pattern, text)
 if search:
     print("Pattern found:", search.group())     //search.group() returns the exact substring of text that matched pattern.
-else:
+else:                                      //If you call .group() with no arguments (or with argument 0), you get the entire substring of the target string that matched the pattern.
     print("Pattern not found")
 ---------------------------------------------------------------------
 With a raw string (prefix r or R), the literal tells Python: “Don’t interpret backslashes as the start of escape sequences — treat them more literally.” 
